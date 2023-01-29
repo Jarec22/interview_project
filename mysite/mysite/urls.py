6853b63d6bda3c27bdf522003289afda49f0794b
Jaroslav Hudacky <jhp.jarec@gmail.com>
2023-01-29 15:16:18 +0000

    Added Django Debug Toolbar

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.staticfiles.storage import staticfiles_storage
-from django.views.generic.base import RedirectView
+import debug_toolbar
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
